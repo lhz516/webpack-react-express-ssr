2e3ad149d71f0c13a97a9fbe49ac409203a352b1
Hanz Luo <lhz0516@gmail.com>
2022-04-25 00:45:09 -0700

    update testing-lib to the latest

--- src/components/home/home.test.js
+++ src/components/home/home.test.js
@@ -5,14 +5,14 @@ import render from '@utils/test-utils'
 import { act } from 'react-dom/test-utils'
 
 describe('Home', () => {
-  it('should increase foo when clicking button', () => {
+  it('should increase foo when clicking button', async () => {
     const { container } = render(<Home />, {
       state: { global: { foo: 0 } },
     })
 
-    act(() => userEvent.click(container.querySelector('.home__add-foo')))
+    await act(() => userEvent.click(container.querySelector('.home__add-foo')))
     expect(container.querySelector('.home__foo')).toHaveTextContent('1')
-    act(() => userEvent.click(container.querySelector('.home__add-foo')))
+    await act(() => userEvent.click(container.querySelector('.home__add-foo')))
     expect(container.querySelector('.home__foo')).toHaveTextContent('2')
   })
 })
