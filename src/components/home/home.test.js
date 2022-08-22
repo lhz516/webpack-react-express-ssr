f7a1ebe8415691bc2b142478fa302213e7c53a46
Hanz Luo <lhz0516@gmail.com>
2022-08-21 23:54:03 -0700

    mock api for tests

--- src/components/home/home.test.js
+++ src/components/home/home.test.js
@@ -2,10 +2,31 @@ import React from 'react'
 import userEvent from '@testing-library/user-event'
 import Home from './home'
 import { createMockedApi, renderWithProvider } from '@utils/test-utils'
-import { act } from 'react-dom/test-utils'
 import { screen } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+const handlers = [
+  rest.get('http://localhost:3000/api/pokemons', (req, res, ctx) => {
+    return res(ctx.json({ text: 'sample returned text, total count is 3' }))
+  }),
+  rest.get('http://localhost:3000/api/todos', (req, res, ctx) => {
+    return res(ctx.json([{ id: 1, text: 'sample todo' }]), ctx.delay(10))
+  }),
+]
+
+const server = setupServer(...handlers)
 
 describe('Home', () => {
+  // Enable API mocking before tests.
+  beforeAll(() => server.listen())
+
+  // Reset any runtime request handlers we may add during the tests.
+  afterEach(() => server.resetHandlers())
+
+  // Disable API mocking after the tests are done.
+  afterAll(() => server.close())
+
   const renderComponent = () => {
     return renderWithProvider(<Home />, {
       state: {
@@ -19,6 +40,7 @@ describe('Home', () => {
       },
     })
   }
+
   it('should display initial SSR state and following CSR', async () => {
     renderComponent()
 
@@ -38,14 +60,10 @@ describe('Home', () => {
 
     expect(screen.getByTestId('home__foo')).toHaveTextContent('0')
 
-    await act(
-      async () => await userEvent.click(screen.getByTestId('home__add-foo'))
-    )
+    await userEvent.click(screen.getByTestId('home__add-foo'))
     expect(screen.getByTestId('home__foo')).toHaveTextContent('1')
 
-    await act(
-      async () => await userEvent.click(screen.getByTestId('home__add-foo'))
-    )
+    await userEvent.click(screen.getByTestId('home__add-foo'))
     expect(screen.getByTestId('home__foo')).toHaveTextContent('2')
   })
 })
